6b1cee10b612faa6e2390d32da940e53b27ce356
Chris Wein <chriswein12@yahoo.com>
2020-12-06 20:42:24 -0600

    Fixed schema errors

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,31 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
-CREATE TABLE department {
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-};
+CREATE TABLE department (
+    id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE role {
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+    id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DEC UNSIGNED NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-};
+    department_id INTEGER UNSIGNED NOT NULL,
+    INDEX (department_id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE employee {
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+    id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT UNSIGNED NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INT UNSIGNED,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-}
+    role_id INTEGER UNSIGNED NOT NULL,
+    INDEX (role_id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER UNSIGNED,
+    INDEX (manager_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
